2ddaf3e09569735d225745096d3944f7013a3b4c
Ricky Flynn <adiosk8a7x@gmail.com>
2023-01-20 22:20:06 -0900

    Added comments

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
+// set the grid to a default 16 x 16
 let gridSize = 16;
-const container = document.querySelector(".grid");
 
+// setup grid layout
+const container = document.querySelector(".grid");
 container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
+// make divs to fill the grid
 for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
         const newDiv = document.createElement("div");
